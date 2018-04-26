3692327f9b697fe60dd5a4f240a18d23f88d84e5
frederic@auberson.net <frederic@auberson.net>
2018-04-26 22:38:19 +0200

    Now without using AWS SAM

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,6 @@
 pushd . > /dev/null
 cd $(dirname "$0")
 mvn package
-aws cloudformation package --template-file route53-ddns.sam-template.yaml --s3-bucket net.auberson.awsddns --output-template-file route53-ddns.yaml
-aws cloudformation deploy --stack-name ddns --template-file route53-ddns.yaml --parameter-overrides "HostedZoneId=$ZONEID" "UserName=$USERNAME" "Password=$PASSWORD" --capabilities CAPABILITY_IAM
+aws s3 cp target/awsddns-1.0.0.jar s3://net.auberson.awsddns
+aws cloudformation deploy --stack-name ddns --template-file aws-route53-ddns.yaml --parameter-overrides "HostedZoneId=$ZONEID" "UserName=$USERNAME" "Password=$PASSWORD" --capabilities CAPABILITY_IAM
 popd > /dev/null
